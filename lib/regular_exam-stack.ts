e1806d8fe81910e36559f6976fba80142af9b0f3
Velmira Georgieva <104137851+VelmiraPetkova@users.noreply.github.com>
2024-11-24 09:31:39 +0200

    Create API Gateway for File Upload

--- lib/regular_exam-stack.ts
+++ lib/regular_exam-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import {RestApi} from "aws-cdk-lib/aws-apigateway";
+import {aws_apigateway} from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class RegularExamStack extends cdk.Stack {
@@ -14,6 +16,13 @@ export class RegularExamStack extends cdk.Stack {
     });
 
 
+    // API Gateway for File Upload
+    const api: cdk.aws_apigateway.RestApi = new RestApi(this,'FileUploadAPI', {
+      restApiName: 'File Upload Service'
+    });
+
+    const uploadResource: cdk.aws_apigateway.Resource = api.root.addResource('upload');
+    uploadResource.addMethod('POST', new aws_apigateway.LambdaIntegration(fileProcessorLambda));
 
 
   }
